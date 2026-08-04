aca71f88dff0138004b9c1f217bd06545ac96292
Bl4cksqu4re-del <loic.a.volat@gmail.com>
2026-08-04 23:03:40 +0200

    Update webex.js

--- route/webex.js
+++ route/webex.js
@@ -4,10 +4,47 @@ const router = express.Router();
 
 console.log('WEBEX ROUTES LOADED');
 
+/*
+ * TEST
+ */
 router.get('/webex/test', (req, res) => {
   res.json({
     ok: true,
     message: 'Webex fonctionne'
+  });
+});
+
+/*
+ * IMPORTS
+ */
+router.post('/webex/import/group', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'Import groupe'
+  });
+});
+
+router.post('/webex/import/agents', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'Import agents'
+  });
+});
+
+router.post('/webex/import/calls', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'Import appels'
+  });
+});
+
+/*
+ * STATS
+ */
+router.get('/webex/stats', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'Stats Webex'
   });
 });
 
