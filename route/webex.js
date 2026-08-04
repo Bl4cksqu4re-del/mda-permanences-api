5ae415f45cc32026a12bcd166a45c3da6bdabeaa
Bl4cksqu4re-del <loic.a.volat@gmail.com>
2026-08-04 22:47:29 +0200

    Update webex.js

--- route/webex.js
+++ route/webex.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/webex/test', async (req, res) => {
+router.get('/webex/test', (req, res) => {
   res.json({
     ok: true,
     message: 'Webex fonctionne'
