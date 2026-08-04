3f10eaaa96318cc46d20acc65de0ad9f0bee741f
Bl4cksqu4re-del <loic.a.volat@gmail.com>
2026-08-04 22:43:39 +0200

    Update webex.js

--- route/webex.js
+++ route/webex.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+const router = express.Router();
+
+router.get('/webex/test', async (req, res) => {
+  res.json({
+    ok: true,
+    message: 'Webex fonctionne'
+  });
+});
+
+module.exports = router;
